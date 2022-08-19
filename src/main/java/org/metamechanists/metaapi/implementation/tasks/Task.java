1fd9a4c463d8c5705a44b4ea1eddba57441d581a
MetaMechanists <percy.baker.0@gmail.com>
2022-08-20 00:05:02 +0100

    -

--- src/main/java/org/metamechanists/metaapi/implementation/tasks/Task.java
+++ src/main/java/org/metamechanists/metaapi/implementation/tasks/Task.java
@@ -4,18 +4,16 @@ import lombok.Getter;
 
 @Getter
 public class Task {
+
     private final String id;
     private final String name;
-    private final Completer completer;
     private final String[] precursors;
     private final Requirement[] requirements;
     private final Reward[] rewards;
 
-    public Task(String id, String name, Completer completer, String[] precursors, Requirement[] requirements,
-                Reward[] rewards) {
+    public Task(String id, String name, String[] precursors, Requirement[] requirements, Reward[] rewards) {
         this.id = id;
         this.name = name;
-        this.completer = completer;
         this.precursors = precursors;
         this.requirements = requirements;
         this.rewards = rewards;
