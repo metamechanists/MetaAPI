4ea29456811ba41036fe5ccd54ad6c22ba01616f
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2022-08-21 10:35:53 -0500

    Debug x3

--- src/main/java/org/metamechanists/metaapi/listeners/tasks/OnMultiBlockCraft.java
+++ src/main/java/org/metamechanists/metaapi/listeners/tasks/OnMultiBlockCraft.java
@@ -2,13 +2,11 @@ package org.metamechanists.metaapi.listeners.tasks;
 
 import io.github.thebusybiscuit.slimefun4.api.events.MultiBlockInteractEvent;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
-import io.github.thebusybiscuit.slimefun4.core.handlers.MultiBlockInteractionHandler;
 import io.github.thebusybiscuit.slimefun4.core.multiblocks.MultiBlock;
 import io.github.thebusybiscuit.slimefun4.implementation.items.blocks.OutputChest;
 import io.github.thebusybiscuit.slimefun4.libraries.paperlib.PaperLib;
 import lombok.SneakyThrows;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
-
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
@@ -20,7 +18,6 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-
 import org.metamechanists.metaapi.implementation.tasks.Requirement;
 import org.metamechanists.metaapi.implementation.tasks.Task;
 import org.metamechanists.metaapi.implementation.tasks.TaskStorage;
@@ -119,9 +116,9 @@ public class OnMultiBlockCraft implements Listener {
         // Read the items in the output inventory into a list, let the multiblock do its thing, then read the items again
         List<ItemStack> before = getItems(block, dispenser);
         Thread.sleep(100);
-        multiBlock.getSlimefunItem().callItemHandler(
-                MultiBlockInteractionHandler.class,
-                handler -> handler.onInteract(player, multiBlock, block));
+        //multiBlock.getSlimefunItem().callItemHandler(
+        //        MultiBlockInteractionHandler.class,
+        //        handler -> handler.onInteract(player, multiBlock, block));
         List<ItemStack> after = getItems(block, dispenser);
 
         Log.info("4");
